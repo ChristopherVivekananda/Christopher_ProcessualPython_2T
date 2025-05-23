c197eaaca5bdbae1a96a73ef877ae9a7bb2c1c9b
Christopher de Mira <159036588+ChristopherVivekananda@users.noreply.github.com>
2025-05-23 11:03:57 -0300

    Create 2entrega.py

---
+++ 2entrega.py
@@ -0,0 +1,10 @@
+dia = 27
+contador = 0
+while contador <= 100:
+    multiplicador = dia * contador
+    print(contador, "x", dia ,"=", multiplicador)
+    contador +=1
+if multiplicador > 1000:
+    print("maior que 1000")
+else:
+    print("menor que 1000")
